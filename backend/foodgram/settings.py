dbe4eca6aea4a67394fcdc0150c283f45da66d19
Nikita Kazakov <kazakov.ni@yandex.ru>
2023-04-19 01:31:43 +0300

    feat:updated docker images, nginx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='1asfsf32fsdfsdfsd')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['158.160.5.176', 'localhost', '127.0.0.1']
 
 
 # Application definition
